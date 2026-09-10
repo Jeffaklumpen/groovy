46641f6155096b3c14bcf3be7350b55249f89dd6
Jeffaklumpen <150244154+Jeffaklumpen@users.noreply.github.com>
2026-09-11 01:34:42 +0200

    Fix avatar placeholder paths on nested routes

--- js/app.js
+++ js/app.js
@@ -115,11 +115,11 @@ async function updateAuthUI(){
             profileImageMenu.style.backgroundSize='cover';
             profileImageMenu.style.backgroundPosition='center';
         }else{
-            profileImage.style.backgroundImage='url("avatar_placeholder.png")';
+            profileImage.style.backgroundImage='url("/groovy/avatar_placeholder.png")';
             profileImage.style.backgroundSize='cover';
             profileImage.style.backgroundPosition='center';
         
-            profileImageMenu.style.backgroundImage='url("avatar_placeholder.png")';
+            profileImageMenu.style.backgroundImage='url("/groovy/avatar_placeholder.png")';
             profileImageMenu.style.backgroundSize='cover';
             profileImageMenu.style.backgroundPosition='center';
         }
@@ -132,11 +132,11 @@ async function updateAuthUI(){
         profileMenu.classList.remove('open');
         loginPanel.classList.remove('open');
 
-        profileImage.style.backgroundImage='url("avatar_placeholder.png")';
+        profileImage.style.backgroundImage='url("/groovy/avatar_placeholder.png")';
         profileImage.style.backgroundSize='cover';
         profileImage.style.backgroundPosition='center';
         
-        profileImageMenu.style.backgroundImage='url("avatar_placeholder.png")';
+        profileImageMenu.style.backgroundImage='url("/groovy/avatar_placeholder.png")';
         profileImageMenu.style.backgroundSize='cover';
         profileImageMenu.style.backgroundPosition='center';
     }
@@ -1992,7 +1992,7 @@ async function loadTopUsers(){
         avatar.className='user-search-avatar';
 
         avatar.style.backgroundImage='url("'+
-            (user.avatar_url||'avatar_placeholder.png')+
+            (user.avatar_url||'/groovy/avatar_placeholder.png')+
             '")';
 
         avatar.style.backgroundSize='cover';
@@ -2076,7 +2076,7 @@ async function searchUsers(query){
         avatar.className='user-search-avatar';
     
         avatar.style.backgroundImage='url("'+
-            (user.avatar_url||'avatar_placeholder.png')+
+            (user.avatar_url||'/groovy/avatar_placeholder.png')+
             '")';
     
         avatar.style.backgroundSize='cover';
@@ -2285,7 +2285,7 @@ async function loadOtherUserCollection(userId){
     viewedUserAvatar.style.backgroundImage='url("'+
         (profile&&profile.avatar_url
             ?profile.avatar_url
-            :'avatar_placeholder.png')+
+            :'/groovy/avatar_placeholder.png')+
         '")';
 
     viewedUserAvatar.style.backgroundSize='cover';
