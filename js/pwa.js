f0078c9805950f22cfbe690920486200d34223c5
Groovyshelf <150244154+Jeffaklumpen@users.noreply.github.com>
2026-09-16 09:38:51 +0200

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -119,14 +119,14 @@
       var style=document.createElement('link');
       style.id='groovyProfileStyles';
       style.rel='stylesheet';
-      style.href='/css/profile.css?v=5';
+      style.href='/css/profile.css?v=6';
       document.head.appendChild(style);
     }
 
     if(!document.getElementById('groovyProfileScript')){
       var script=document.createElement('script');
       script.id='groovyProfileScript';
-      script.src='/js/profile.js?v=5';
+      script.src='/js/profile.js?v=6';
       document.body.appendChild(script);
     }
   }
